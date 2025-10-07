6f27151b3b145414c14875324beac62b7ce0499d
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-10-07 14:51:05 -0300

    Comentário em js

---
+++ aula4/comentario.js
@@ -0,0 +1,10 @@
+// Escrever um comentário
+console.log('Olá mundo!') //Aqui tem outro comentário
+console.log('Linha 1')
+//console.log('Linha 2')
+console.log('Linha 3')
+console.log('Linha 4')
+//Comentário de linha
+
+/*Comentario longo
+onde pode ser passado por diversas linhas*/
