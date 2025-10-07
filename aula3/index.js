73bee028f21ca0fa7d9bc290b4e6b996a1cd6f6b
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-10-07 11:24:23 -0300

    Conhecendo o console.log

---
+++ aula3/index.js
@@ -0,0 +1,10 @@
+var num = 5
+var rec = Number(num)
+console.log(rec)
+console.log('Luiz "otavio"'); //String = Texto
+console.log("Luiz 'otavio'"); //string
+console.log(`'Luiz' "otavio"`); //String
+
+
+console.log(54) //Number = Numero
+console.log(123456, 15455455.8556556, 'Matheus Vasoncelos') //String e Number juntos
