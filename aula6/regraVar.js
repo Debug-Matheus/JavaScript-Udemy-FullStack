8307a55d300acaad23893fb3cfcada9ec2509d75
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-10-08 11:40:08 -0300

    Variáveis com let

---
+++ aula6/regraVar.js
@@ -0,0 +1,8 @@
+// Não podemos criar variaveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variavel com um numero
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilize VAR, UTILIZE LET
