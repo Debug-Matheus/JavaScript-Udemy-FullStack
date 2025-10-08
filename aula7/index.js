f901370de83c59584b08f6507e18098086030e68
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-10-08 12:41:37 -0300

    Constante com const

---
+++ aula7/index.js
@@ -0,0 +1,17 @@
+// const nome = 'Matheus'
+// console.log(nome)
+// nome = 'Felipe'
+// console.log(nome)
+
+// const valor;
+
+const primeiroNumero = 10;
+const segundoNumero = 8;
+const resultado = primeiroNumero * segundoNumero;
+console.log('A multiplicação entre', primeiroNumero, 'e', segundoNumero,'é:', resultado);
+const resultadoDuplicado = resultado * 2;
+console.log('Resultado duplicado: ', resultadoDuplicado);
+let resultadoTriplicado = resultado * 3;
+console.log('Resultado triplicado: ',resultadoTriplicado);
+resultadoTriplicado = resultadoTriplicado + 10;
+console.log('Ganhaou mais 10 no resultado triplicado: ', resultadoTriplicado)
---
+++ aula7/regraConst.js
@@ -0,0 +1,9 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um numero
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// Não utilize VAR, UTILIZE CONST
+// Não pode declarar uma constante sem iniciar ela
