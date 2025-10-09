4a2af3cc237a99b5e34851caba91a258a2bf79e5
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-10-09 12:15:21 -0300

    Aritmetico + incremento + atribuição

---
+++ aula10/aritmetico.js
@@ -0,0 +1,25 @@
+/* Aritméticos 
+* + -> adição e concatenação
+* - / * 
+* ** -> Potenciação
+* % -> resto da divisão
+*/
+
+/* Ordem da PRECEDÊNCIA
+()
+**
+* / %
++ -
+*/
+const num1 = 5;
+const num2 = 10;
+const num3 = "20";
+// console.log(num1 + num2)
+// console.log(num2 + num3)
+// console.log(num2 - num1)
+// console.log(num2 / num1)
+// console.log(num1 * num2)
+// console.log(num1 ** num2)
+// console.log(num2 % num1)
+console.log(num2 + num1 * num2)
+console.log((num2 +num1) + num2 ** 2)
---
+++ aula10/incrementoAtribuicao.js
@@ -0,0 +1,21 @@
+//Incremento : ++
+//Decremento: -- 
+
+let contador = 0;
+let passo = 2;
+contador += passo;
+console.log(contador);
+contador+=passo;
+console.log(contador);
+contador += passo;
+console.log(contador);
+
+//Operadores de Atribuição
+contador *= passo
+console.log(contador);
+contador /= passo
+console.log(contador);
+contador **= passo
+console.log(contador);
+contador %= passo
+console.log(contador);
