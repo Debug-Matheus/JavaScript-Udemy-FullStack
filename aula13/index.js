64edb0e8af7dd16e8bc768420f9f863e4bf4465b
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-10-11 14:10:26 -0300

    Sobre strings

---
+++ aula13/index.js
@@ -0,0 +1,23 @@
+//               012345678910 
+let umaString = 'Um textolioppp'
+// console.log(umaString[3])
+// console.log(umaString.charAt(1))
+// console.log(umaString.charAt(8))
+// console.log(umaString[8])
+// console.log(umaString[-1])
+// console.log(umaString.length)
+// console.log(umaString.indexOf('U'))
+
+// console.log(umaString.concat(' em', ' um', ' lindo dia'))
+
+// console.log(umaString.concat(' em um lindo dia'))
+
+// console.log(umaString + ' em um lindo dia')
+
+// console.log(`${umaString} em um lindo dia`)
+
+// console.log(umaString.indexOf('o', 3))
+// console.log(umaString.lastIndexOf('o',3))
+// console.log(umaString.lastIndexOf('o',12))
+// console.log(umaString.search('x'));
+console.log(umaString.replace(/Um/, ' Outro'))
---
+++ aula13/index2.js
@@ -0,0 +1,13 @@
+//               012345678910
+let umaString = 'O rato roeu a roupa do rei de roma'; 
+
+// console.log(umaString.replace(/r/, '#'))
+// console.log(umaString.replace(/r/g, '#'))
+console.log('O numero de indice da variavel é: ' + umaString.length)
+console.log(umaString.slice(2,6))
+console.log(umaString.slice(-4))
+console.log(umaString.slice(-4, -1))
+console.log(umaString.split(' ', 3))
+console.log(umaString.toUpperCase())
+console.log(umaString.toLowerCase())
+
