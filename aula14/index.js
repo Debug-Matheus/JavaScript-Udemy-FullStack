5dc8648742d55404e1b879952b63c1c1084fff1d
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-10-14 13:08:47 -0300

    Mais entendimento com numbers em JS

---
+++ aula14/index.js
@@ -0,0 +1,21 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2
+num1 += num2
+num1 += num2
+num1 = Number(num1.toFixed(2))
+console.log(num1)
+console.log(Number.isInteger(num1))
+
+// console.log(num1.toFixed(2))
+// console.log(Number.isInteger(1.00))
+// console.log(Number.isInteger(num1))
+// console.log(num1.toString() + num2)
+// console.log(typeof(num1));
+// console.log(num1.toString(2))
+// console.log(num1.toFixed(2))
+// console.log(Number.isInteger(num1))
+// let temp = num1 * '5'
+// console.log(Number.isNaN(temp))
+
