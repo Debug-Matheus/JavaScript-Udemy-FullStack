79986ead16563b072ed19ef2dd488aebc7211690
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-10-15 15:16:01 -0300

    Exercicios com numbers e Math

---
+++ aula15/index.js
@@ -0,0 +1,16 @@
+let num1 = 9.51;
+// let num2 = Math.floor(num1)
+// let num2 = Math.ceil(num1)
+// let num2 = Math.round(num1)
+// console.log(num2)
+// console.log(Math.max(1,6,85,755,-4,785,-845,22,12,3,5,4))
+// console.log(Math.min(1,6,85,755,-4,785,-845,22,12,3,5,4))
+// console.log(Math.random() * (10 - 5) + 5)
+// console.log(Math.round(Math.random() * (10 - 5) + 5))
+// const aleatorio = Math.random()* (10-5) + 5
+// console.log(Math.round(aleatorio))
+// console.log(Math.PI.toFixed(2))
+console.log(Math.pow(2, 10))
+console.log(2**10)
+console.log(9 ** 0.5)
+// console.log(100 / 0)
