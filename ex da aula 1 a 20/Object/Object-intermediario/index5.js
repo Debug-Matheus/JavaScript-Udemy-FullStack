a936e78e6e28f66a09df1b2cf56946d5a9fd1c1e
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-10-28 14:41:38 -0300

    incremento de logica com objeto e função

--- ex da aula 1 a 20/Object/Object-intermediario/index5.js
+++ ex da aula 1 a 20/Object/Object-intermediario/index5.js
@@ -1,7 +1,15 @@
-const geradorAleatorio = {
-    aleatorio: function(min,max){
-        return Math.floor(Math.random() * (max-min+1) + min)
-    }
+// const geradorAleatorio = {
+//     aleatorio: function(min,max){
+//         return Math.floor(Math.random() * (max-min+1) + min)
+//     }
+// }
+// numero1 = geradorAleatorio.aleatorio(5,20)
+// console.log(numero1)
+function aleatorio(max,min){
+    return Math.floor(Math.random()* (max-min +1 ) + min)
 }
-numero1 = geradorAleatorio.aleatorio(5,20)
-console.log(numero1)
+const geradorAleatorio = {
+    numeroAleatorio: aleatorio(5,100)
+}
+const numero = geradorAleatorio.numeroAleatorio;
+console.log(numero)
