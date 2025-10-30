1cf947fde954e7e20f732fc88686bb93c96b5429
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-10-30 17:02:19 -0300

    Operador de comparação + operador Lógico

---
+++ Javascript Logica de Programação/aula21/index.js
@@ -0,0 +1,34 @@
+/*
+Operadores de Comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a 
+== igualdade (checa apenas valor) -> não recomendado
+=== igualdade estrita (checa valor e tipo)
+!= diferente (checa apenas valor) -> não recomendado
+!== diferente estrito (checa valor e tipo)
+*/ 
+// const comp = 10 > 5;
+// console.log(comp)
+
+// const comp = 10 >= 10;
+// console.log(comp)
+
+// const comp = 10 < 11;
+// console.log(comp)
+
+// const num1 = 10;
+// const num2 = '10'; 
+
+// console.log(num1 == num2)
+
+// const num1 = 10;
+// const num2 = '10'; 
+
+// console.log(num1 === num2)
+
+const num1 = 10;
+const num2 = '10'; 
+
+console.log(num1 !== num2)
---
+++ Javascript Logica de Programação/aula22/index.js
@@ -0,0 +1,31 @@
+/*
+Operadores Lógicos 
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+//AND
+// console.log(true && true && false)
+// console.log( true && true && true)
+
+//OR
+// console.log(true || false)
+// console.log(false || false)
+
+//exercicio
+// const login = 'Matheus'
+// const senha = '1234'
+// const vaiLogar = login === 'Matheus' && senha === '12345'
+// console.log(vaiLogar)
+
+//exercicio
+// const login = 'Matheus'
+// const senha = '1234'
+// const vaiLogar = login === 'Matheus' || senha === '12345'
+// console.log(vaiLogar)
+
+//NOT
+console.log(!false)
+console.log(!true)
+
+console.log(!!!true)
