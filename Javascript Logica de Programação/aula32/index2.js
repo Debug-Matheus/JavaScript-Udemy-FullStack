cdac1c2391fa13667865d2d74ef11f1a88df5a40
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-11-11 17:44:05 -0300

    Atribuição via desestruturação (Arrays)

---
+++ Javascript Logica de Programação/aula32/index2.js
@@ -0,0 +1,6 @@
+const numeros = [1,2,3,4,5,6,7];
+const primeiroNumero = numeros[0]
+const segundoNumero = numeros[1]
+const terceiroNumero = numeros[2]
+const quartoNumero = numeros[3]
+console.log(primeiroNumero, segundoNumero, terceiroNumero, quartoNumero)
---
+++ Javascript Logica de Programação/aula32/index5.js
@@ -0,0 +1,4 @@
+//...rest , ...spread
+const numeros = [1,2,3,4,5,6,7];
+const [um, , três, , cinco, , sete] = numeros
+console.log(um, três, cinco, sete)
