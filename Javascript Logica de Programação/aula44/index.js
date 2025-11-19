2c2eb0ebcc26117ab3d2c8850356c880d665841e
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-11-19 15:44:39 -0300

    Tratando e lançando erros (try, catch, throw)

---
+++ Javascript Logica de Programação/aula44/index.js
@@ -0,0 +1,12 @@
+//Tratando e lançando erros (try, catch, throw)
+
+
+
+
+try{
+    console.log(naoExisto)
+}catch(e /*error, err*/){
+    console.log('naoExisto não existe.')
+    console.log(e)
+    
+}
---
+++ Javascript Logica de Programação/aula44/index4.js
@@ -0,0 +1,12 @@
+function soma (x,y){
+    if(typeof x !== 'number'|| typeof y !== 'number'){
+        throw new ReferenceError ('x e y precisam ser numeros.')
+    }
+    return x + y
+}
+
+try{
+    console.log(soma(8,'1'))
+}catch(err){
+    console.log(err)
+}
