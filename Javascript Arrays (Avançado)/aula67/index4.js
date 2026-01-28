99315debd1d1f098590c82b00ad06fd2e7bc03ab
Matheus Vasconcelos <marco-til1@hotmail.com>
2026-01-28 18:08:22 -0300

    Reduce - Reduzindo o array

---
+++ Javascript Arrays (Avançado)/aula67/index4.js
@@ -0,0 +1,17 @@
+//Reduce
+
+//Some todos os numeros (reduce)
+//Retorne um array com os pares (filter)
+//Retorne um array com o dobro dos valores (map)
+
+
+//O acumulador caso não defina como 0, será o primeiro valor do array. 
+const numeros = [5,50,80,1,2,3,5,8,7,11,15,22,77]
+const  total = numeros.reduce(function(acumulador, valor, indice, array){
+
+    if(valor % 2 ===0){
+        acumulador += valor
+    }
+    return acumulador
+},0)
+console.log(total)
---
+++ Javascript Arrays (Avançado)/aula67/index5.js
@@ -0,0 +1,17 @@
+//Retorne a pessoa mais velha
+
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Leticia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 77}
+]
+const pessoaVelha = pessoas.reduce(function(acumulador, valor){
+    if(acumulador.idade > valor.idade){
+        return acumulador
+    }
+    return valor
+})
+console.log(pessoaVelha)
