442ac2ea0a51841236ad2bb362d93d3f61e14fa7
Matheus Vasconcelos <marco-til1@hotmail.com>
2026-03-24 21:24:43 -0300

    Criando Classes

---
+++ Javascript Classes - Programação Orientada a Objetos - POO/aula82/index.js
@@ -0,0 +1,23 @@
+//Criando Classes
+//Comparando com Constructor Function
+
+class Pessoa{
+    constructor(nome,sobrenome){
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+    falar(){
+        console.log(this.nome + 'está falando')
+    }
+}
+function Pessoa2(nome,sobrenome){
+    this.nome = nome
+    this.sobrenome = sobrenome
+}
+Pessoa2.prototype.falar = function(){
+    console.log(this.nome + 'está falando')
+}
+const p1 = new Pessoa('Matheus', 'Vasconcelos')
+const p2 = new Pessoa2('Matheus', 'Vasconcelos')
+console.log(p1)
+console.log(p2)
