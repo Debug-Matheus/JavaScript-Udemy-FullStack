17ba45150ce7b9f27b23eaaaacf3d3e1e9a3347d
Matheus Vasconcelos <marco-til1@hotmail.com>
2026-04-27 16:27:38 -0300

    Promises

---
+++ JavaScript assícrono - Promises, Ajax, Fetch e AsyncAwait/aula86/index.js
@@ -0,0 +1,34 @@
+function rand(min,max){
+    min*= 1000;
+    max*= 1000
+    return Math.floor(Math.random() * (max-min) + min)
+}
+
+function esperaAi(msg,tempo){
+    return new Promise((resolve,reject)=>{
+        if(typeof msg !== 'string') reject(new Error('ERRO'))
+        setTimeout(()=>{
+            resolve(msg)
+        },tempo)
+    })
+}
+esperaAi('Conexão com o BD', rand(1,3))
+    .then(resposta =>{
+        console.log(resposta)
+        return esperaAi('Buscando dados da Base', rand(1,3))
+    })
+    .then(resposta=>{
+        console.log(resposta)
+        return esperaAi(1123123, rand(1,3))
+    })
+    .then(resposta =>{
+        console.log(resposta)
+    })
+    .then(resposta =>{
+        console.log('Exibe dados na tela')
+    })
+    .catch(e =>{
+        console.log('Error:',e)
+    })
+
+console.log('Isso aqui será exibido antes de qualquer promise')
